ab68f3a521c4309cee7b493c308ccb6000e562d5
Ukpeh Michael <marcelukpeh@gmail.com>
2023-10-18 11:05:40 +0100

    patch

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -50,7 +50,7 @@ const geoValidate = asyncHandler(async (req, res, next) => {
         latitude: process.env.MY_OFFICE_LATITUDE,
         longitude: process.env.MY_OFFICE_LONGITUDE,
       },
-      500
+      0
     );
     console.log(result);
     if (result) {
